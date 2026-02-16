4ebe4922638e306ae087b822315725fc6ae96a75
terminal-case <gsurr326@tuta.io>
2026-02-16 17:55:32 -0500

    allow shared buffer handling

--- src/zcgltf.zig
+++ src/zcgltf.zig
@@ -103,7 +103,7 @@ pub fn appendMeshPrimitive(
             assert(buffer_view.buffer.data != null);
 
             assert(accessor.stride == buffer_view.stride or buffer_view.stride == 0);
-            assert(accessor.stride * accessor.count == buffer_view.size);
+            assert(accessor.offset + accessor.stride * accessor.count <= buffer_view.size);
 
             const data_addr = @as([*]const u8, @ptrCast(buffer_view.buffer.data)) +
                 accessor.offset + buffer_view.offset;
